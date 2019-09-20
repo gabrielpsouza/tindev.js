bc74726237b0362b529efe7ce3df0e2a25ff599b
Gabriel Pedro Souza <gabriel.souza@corp.eptv>
2019-09-20 12:54:28 -0300

    Finalizando Api

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+
 
 const server = express();
 
+mongoose.connect("mongodb+srv://admin:G@12345678@cluster0-bivna.mongodb.net/TinDev?retryWrites=true&w=majority", { useNewUrlParser: true });
+ 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
